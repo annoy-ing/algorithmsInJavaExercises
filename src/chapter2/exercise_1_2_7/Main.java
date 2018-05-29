37ae5ca1634fedc9cd3871886a03b8cccfc6d19b
kuro <olegrumblerstain@yahoo.com>
2018-05-29 15:55:45 +0300

    Added ex1.2.7. Function returns String backward

---
+++ src/chapter2/exercise_1_2_7/Main.java
@@ -0,0 +1,19 @@
+package chapter2.exercise_1_2_7;
+
+public class Main {
+
+    public static String mystery(String s) {
+        int N = s.length();
+        if (N <= 1) return s;
+        String a = s.substring(0, N/2);
+        String b = s.substring(N/2, N);
+        return mystery(b) + mystery(a);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(mystery("mystery"));
+        
+
+    }
+}
